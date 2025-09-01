17ae4ea4a0dbd666caad9ba7961918d2b5d09e88
kbprashant <41023256+kbprashant@users.noreply.github.com>
2025-09-01 21:54:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
+    allowedHosts: [
+      'z4kossco4gs0sowcc8ssscow.168.231.121.95.sslip.io'  // add your Coolify domain here
+    ]
   }
 })
