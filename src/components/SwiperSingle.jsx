79065c37bd0bed68e375c2b6f32064755ddd6c89
Prasanth B <tbkprashant@gmail.com>
2025-09-10 12:43:26 +0530

    10/09 8

--- src/components/SwiperSingle.jsx
+++ src/components/SwiperSingle.jsx
@@ -11,7 +11,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "./css/caruseltwo.css";
 import "swiper/swiper-bundle.css";
 
-const API_PUBLIC = import.meta.env.VITE_API_PUBLIC_BASE_URL || "http://localhost:8080/api";
+const API_PUBLIC = import.meta.env.VITE_API_PUBLIC_BASE_URL || "/api";
 
 // Build absolute URL for images returned as "/uploads/…"
 function toAbsoluteUrl(pathOrUrl) {
