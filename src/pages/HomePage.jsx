a9439fa700b1cc3599571ac602a6171e65072a3a
Prasanth B <tbkprashant@gmail.com>
2025-09-12 12:11:34 +0530

    12/09 3

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./css/homepage.css";
 
 import { SplitStringUsingRegex } from "../utils/SplitStringUsingRegex";
